fe83d66b48644424cabcdc0dc60f7b764b414466
Legomegger <legomegakst@gmail.com>
2021-08-30 14:18:14 +0600

    pretty routes

--- routes/api.php
+++ routes/api.php
@@ -24,10 +24,10 @@ Route::group([
     'middleware' => 'api',
     'prefix' => 'auth'
 ], function ($router) {
-    Route::post('login', [AuthController::class, 'login']);
-    Route::post('logout', [AuthController::class, 'logout']);
-    Route::post('refresh', [AuthController::class, 'refresh']);
-    Route::post('me', [AuthController::class, 'me']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::post('/me', [AuthController::class, 'me']);
     Route::post('/register', [AuthController::class, 'register']);
 });
 
